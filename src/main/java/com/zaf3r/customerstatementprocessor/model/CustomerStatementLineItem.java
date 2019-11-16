9eca78e7ba02fe3b92df79bed39b46e71436773b
zafer <zt14183@gmail.com>
2019-11-17 00:25:36 +0100

    added functionality for reading CSV from hardcoded path

--- src/main/java/com/zaf3r/customerstatementprocessor/model/CustomerStatementLineItem.java
+++ src/main/java/com/zaf3r/customerstatementprocessor/model/CustomerStatementLineItem.java
@@ -1,14 +1,15 @@
 package com.zaf3r.customerstatementprocessor.model;
 
 public class CustomerStatementLineItem {
-    private int transactionReference;
+    private long transactionReference;
     private String accountNumber;
     private double startBalance;
     private double mutation;
     private double endBalance;
     private String description;
+    private boolean failedRecord;
 
-    public CustomerStatementLineItem(int transactionReference, String accountNumber, double startBalance, double mutation, double endBalance, String description) {
+    public CustomerStatementLineItem(long transactionReference, String accountNumber, double startBalance, double mutation, double endBalance, String description) {
         this.transactionReference = transactionReference;
         this.accountNumber = accountNumber;
         this.startBalance = startBalance;
@@ -17,7 +18,7 @@ public class CustomerStatementLineItem {
         this.description = description;
     }
 
-    public int getTransactionReference() {
+    public long getTransactionReference() {
         return transactionReference;
     }
 
@@ -39,5 +40,26 @@ public class CustomerStatementLineItem {
 
     public String getDescription() {
         return description;
+    }
+
+    public boolean isFailedRecord() {
+        return failedRecord;
+    }
+
+    public void setFailedRecord(boolean failedRecord) {
+        this.failedRecord = failedRecord;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerStatementLineItem{" +
+                "transactionReference=" + transactionReference +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", startBalance=" + startBalance +
+                ", mutation=" + mutation +
+                ", endBalance=" + endBalance +
+                ", description='" + description + '\'' +
+                ", failedRecord=" + failedRecord +
+                '}';
     }
 }
--- src/main/java/com/zaf3r/customerstatementprocessor/resource/CustomerStatementProcessorController.java
+++ src/main/java/com/zaf3r/customerstatementprocessor/resource/CustomerStatementProcessorController.java
@@ -16,32 +16,42 @@ public class CustomerStatementProcessorController implements CommandLineRunner {
 
     public final int TRANSACTION_REFERENCE_COLUMN = 0;
     public final int ACCOUNT_NUMBER_COLUMN = 1;
-    public final int START_BALANCE_COLUMN = 2;
-    public final int MUTATION_COLUMN = 3;
-    public final int DESCRIPTION_COLUMN = 4;
+    public final int DESCRIPTION_COLUMN = 2;
+    public final int START_BALANCE_COLUMN = 3;
+    public final int MUTATION_COLUMN = 4;
     public final int FINAL_BALANCE_COLUMN = 5;
 
     public List<CustomerStatementLineItem> readCustomerStatement() {
-        File file = new File("C:\\Users\\zafer.torun\\projects\\cgi\\rabo-assignment\\customer-statement-processor\\src\\main\\resources\\customerstatements\\records.csv");
+        String fileLocation = new File("src/main/resources/customerstatements/records.csv").getAbsolutePath();
+        List<CustomerStatementLineItem> lineItemList = new ArrayList<>();
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File(fileLocation)))) {
+            reader.readLine();
             String line;
             while((line = reader.readLine()) != null) {
-                String[] splitRow = line.split(",");
-
+                lineItemList.add(loadCommaSeparatedLineIntoModel(line));
             }
-
-
         } catch (IOException e) {
             System.out.println(e.getClass()+ " : " + e.getMessage());
         }
-
-        return new ArrayList<>();
+        System.out.println(lineItemList);
+        return lineItemList;
     }
 
+    public CustomerStatementLineItem loadCommaSeparatedLineIntoModel(String line) {
+        String[] splitLine = line.split(",");
+        long transactionRef = Long.parseLong(splitLine[TRANSACTION_REFERENCE_COLUMN]);
+        String accountNumber = splitLine[ACCOUNT_NUMBER_COLUMN];
+        double startBalance = Double.parseDouble(splitLine[START_BALANCE_COLUMN]);
+        double mutation = Double.parseDouble(splitLine[MUTATION_COLUMN]);
+        String description = splitLine[DESCRIPTION_COLUMN];
+        double finalBalance = Double.parseDouble(splitLine[FINAL_BALANCE_COLUMN]);
+
+        return new CustomerStatementLineItem(transactionRef,accountNumber,startBalance,mutation,finalBalance,description);
+    }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         readCustomerStatement();
     }
 }
